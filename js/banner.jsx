7d663b086a18368c95be07e150ed138ac9ffaca2
Simon <simonfong6@gmail.com>
2018-12-17 18:03:25 -0800

    Banner image

---
+++ js/banner.jsx
@@ -0,0 +1,14 @@
+'use strict';
+
+class Banner extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    return (
+      <img className="img-responsive" src="/images/fusion-xix-banner.jpg" alt="Fusion Banner"/>
+    );
+  }
+}
