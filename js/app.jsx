4b61ab2a757d053b92eec74aae556629ce328d88
Simon <simonfong6@gmail.com>
2018-12-17 19:11:52 -0800

    Social media footer

--- js/app.jsx
+++ js/app.jsx
@@ -11,7 +11,7 @@ class App extends React.Component {
       <div>
         <NavBar/>
         <Banner/>
-        <SocialMediaBar/>
+        <Footer/>
       </div>
       
     );
--- js/social_media_bar.jsx
+++ js/social_media_bar.jsx
@@ -9,23 +9,25 @@ class SocialMediaBar extends React.Component {
 
     return (
       <div className="social_media_bar">
-        <SocialMedia
-          href="http://www.facebook.com/fusionhhdc"
-          company="facebook"
-        />
-        <SocialMedia
-          href="https://instagram.com/fusionhiphop"
-          company="instagram"
-          square={false}
-        />
-        <SocialMedia
-          href="http://twitter.com/fusionhiphop?lang=en"
-          company="twitter"
-        />
-        <SocialMedia
-          href="http://www.youtube.com/channel/UCCbFcX7YCu-4NWZNuPEFggw"
-          company="youtube"
-        />
+        <ul>
+          <SocialMedia
+            href="http://www.facebook.com/fusionhhdc"
+            company="facebook"
+          />
+          <SocialMedia
+            href="https://instagram.com/fusionhiphop"
+            company="instagram"
+            square={false}
+          />
+          <SocialMedia
+            href="http://twitter.com/fusionhiphop?lang=en"
+            company="twitter"
+          />
+          <SocialMedia
+            href="http://www.youtube.com/channel/UCCbFcX7YCu-4NWZNuPEFggw"
+            company="youtube"
+          />
+        </ul>
       </div>
     );
   }
