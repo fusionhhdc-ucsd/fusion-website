e5933e65a07d737c9556ef8712e5397ae3a992c1
Simon <simonfong6@gmail.com>
2018-12-17 18:44:09 -0800

    Made navbar adjustable

--- js/navbar.jsx
+++ js/navbar.jsx
@@ -11,16 +11,24 @@ class NavBar extends React.Component {
       <nav className="navbar navbar-inverse">
         <div className="container-fluid">
           <div className="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>                        
+            </button>
             <a className="navbar-brand" href="/index.html">Fusion</a>
           </div>
-          <ul className="nav navbar-nav">
-            <li className="active"><a href="/index.html">Home</a></li>
-            <li><a href="/about.html">About</a></li>
-            <li><a href="/staff.html">Our Staff</a></li>
-          </ul>
-          <a href="https://tinyurl.com/applyfusionxix" target="_blank">
-            <button className="btn btn-primary navbar-btn">Apply</button>
-          </a>
+          <div class="collapse navbar-collapse" id="myNavbar">
+            <ul className="nav navbar-nav">
+              <li className="active"><a href="/index.html">Home</a></li>
+              <li><a href="/about.html">About</a></li>
+              <li><a href="/staff.html">Our Staff</a></li>
+            </ul>
+            <a href="https://tinyurl.com/applyfusionxix" target="_blank">
+              <button className="btn btn-primary navbar-btn">Apply</button>
+            </a>
+          </div>
+          
         </div>
       </nav>
     );
