84c1af892f3c991792c349654ea8058cbabd8222
Simon <simonfong6@gmail.com>
2019-01-01 22:29:13 -0800

    Added margin to bottom

--- js/banner.jsx
+++ js/banner.jsx
@@ -9,7 +9,7 @@ class Banner extends React.Component {
 
     return (
 
-      <div id="demo" className="carousel slide" data-ride="carousel">
+      <div id="demo" className="carousel slide" data-ride="carousel" style={{"margin-bottom":"2em"}}>
 
         <ul className="carousel-indicators">
           <li data-target="#demo" data-slide-to="0" className="active"></li>
