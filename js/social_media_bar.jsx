4f8bd3ceba5f865a41b1a5f08da4961862139fd5
Simon <simonfong6@gmail.com>
2019-01-04 20:02:25 -0800

    Make social media bar cols responsive

--- js/social_media_bar.jsx
+++ js/social_media_bar.jsx
@@ -9,23 +9,35 @@ class SocialMediaBar extends React.Component {
 
     return (
       <div className="social_media_bar">
-          <SocialMedia
-            href="http://www.facebook.com/fusionhhdc"
-            company="facebook"
-          />
-          <SocialMedia
-            href="https://instagram.com/fusionhiphop"
-            company="instagram"
-            square={false}
-          />
-          <SocialMedia
-            href="http://twitter.com/fusionhiphop?lang=en"
-            company="twitter"
-          />
-          <SocialMedia
-            href="http://www.youtube.com/channel/UCCbFcX7YCu-4NWZNuPEFggw"
-            company="youtube"
-          />
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <SocialMedia
+                href="http://www.facebook.com/fusionhhdc"
+                company="facebook"
+              />
+            </div>
+            <div className="col">
+              <SocialMedia
+                href="https://instagram.com/fusionhiphop"
+                company="instagram"
+                square={false}
+              />
+            </div>
+            <div className="col">
+              <SocialMedia
+                href="http://twitter.com/fusionhiphop?lang=en"
+                company="twitter"
+              />
+            </div>
+            <div className="col">
+              <SocialMedia
+                href="http://www.youtube.com/channel/UCCbFcX7YCu-4NWZNuPEFggw"
+                company="youtube"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
