61f8ca0111422106928f40a5f2bf4202c590e003
Simon <simonfong6@gmail.com>
2019-01-04 16:41:36 -0800

    Change history from bullets to paragraphs

--- js/history_timeline.jsx
+++ js/history_timeline.jsx
@@ -10,8 +10,8 @@ class HistoryTimeline extends React.Component {
     return (
       <div>
         <h2>History</h2>
-        <ul>
-          <li>
+        <div>
+          <p>
             In 2018, FUSION XVIII will mark FUSION’s eighteenth iteration with
             an established history. As an organization, FUSION began in 1999
             alongside of MASA and kunFUSION. By 2000, MASA and 220, its
@@ -22,9 +22,9 @@ class HistoryTimeline extends React.Component {
             peak years of America’s Best Dance Crew and public interest in the
             hip hop dance community as a whole, FUSION attracted 3000 audience
             members and performers, one of the largest attendance number at any
-            hip hop dance event anywhere. 
-          </li>
-          <li>
+            hip hop dance event anywhere.
+          </p>
+          <p>
             Unfortunately, in 2012, FUSION XIII was canceled to the heartbreak
             of many older MASA members and dancers in the hip hop community as
             a whole due to venue problems. Additionally, MASA and 220 mutually
@@ -34,8 +34,8 @@ class HistoryTimeline extends React.Component {
             dance community, while 220 has become respected as a rising force
             with multiple first and second place finishes at other hip hop
             dance competitions. 
-          </li>
-          <li>
+          </p>
+          <p>
             As an organization, FUSION began in 1999 alongside of MASA and
             kunFUSION. Originally started as a collaboration between MASA and
             220, its competitive dance team, FUSION I was held at PC West
@@ -51,8 +51,8 @@ class HistoryTimeline extends React.Component {
             have developed a strong relationship with the organization and
             shared many wonderful memories. FUSION XIX will mark FUSION’s
             twentieth iteration with this established history.
-          </li>
-        </ul>
+          </p>
+        </div>
       </div>
       
     );
