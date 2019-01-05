6ddf589d919df23486e0f7771dc39c26851d37fb
Simon <simonfong6@gmail.com>
2019-01-04 17:39:10 -0800

    Removed 'Coming soon to golden hall'

--- js/location_map.jsx
+++ js/location_map.jsx
@@ -15,7 +15,6 @@ class LocationMap extends React.Component {
                 'margin-right': 'auto'
         }}
       >
-        Coming soon to Golden Hall...<br/>
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3356.8111694209247!2d-117.16551168481942!3d32.71764508099002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80d9543fc8a194cd%3A0x64836d4d79a1c138!2sGolden+Hall!5e0!3m2!1sen!2sus!4v1545120776575"
           width="400"
