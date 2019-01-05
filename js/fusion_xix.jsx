6fcc389d6072989b91e7f1bb726c72e3d081550d
Simon <simonfong6@gmail.com>
2019-01-04 17:34:03 -0800

    Added fusion xix info

--- js/fusion_xix.jsx
+++ js/fusion_xix.jsx
@@ -11,6 +11,34 @@ class FusionXIX extends React.Component {
       <div>
         <NavBar/>
         <Banner/>
+        <div
+          className='container'
+          style={{
+            'width': '70%'
+          }}
+        >
+          <h1>
+            FUSION XIX
+          </h1>
+          <p
+            style={{
+              'font-weight': 'bold'
+            }}
+          >
+            This year’s event info:
+          </p>
+          <ul>
+            <li>
+              FUSION Dance Competition (FUSION XIX)
+            </li>
+            <li>
+              Date: April 20th, 2019
+            </li>
+            <li>
+              Location: Golden Hall
+            </li>
+          </ul>
+        </div>
         <Footer/>
       </div>
       
