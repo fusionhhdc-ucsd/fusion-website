6e26ec4c879bdd10ce866dd2ab5f7084c2c3e71d
Simon <simonfong6@gmail.com>
2019-01-13 23:05:06 -0800

    Added staff application closed message

--- js/Apply.jsx
+++ js/Apply.jsx
@@ -11,6 +11,16 @@ class Apply extends React.Component {
       <div>
         <NavBar/>
         <Banner/>
+        <h1 className="text-center">
+          Applications are now closed for FUSION XIX Hip Hop Dance Competition.
+          Please come back in December 2019 for FUSION XX applications! We also
+          post our applications on our Facebook &nbsp;
+            <a
+              href="http://www.facebook.com/fusionhhdc"
+            >
+            page
+            </a>.
+        </h1>
         <Footer/>
       </div>
       
