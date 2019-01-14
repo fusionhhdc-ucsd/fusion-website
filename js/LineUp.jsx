dbea521beead4cfc74034f3e4e00b3a7d6448e2e
Simon <simonfong6@gmail.com>
2019-01-14 00:55:59 -0800

    Moved LineUp into compponent

---
+++ js/LineUp.jsx
@@ -0,0 +1,22 @@
+'use strict';
+
+class LineUp extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    return (
+      <div>
+        <h2>
+          Line Up
+        </h2>
+        <p>
+          To be announced...
+        </p>
+      </div>
+      
+    );
+  }
+}
