3346f501418d4acf70b149a1531d380c143a2c70
Simon <simonfong6@gmail.com>
2019-01-13 23:54:09 -0800

    Added kickoff info

--- js/Kickoff.jsx
+++ js/Kickoff.jsx
@@ -11,6 +11,39 @@ class Kickoff extends React.Component {
       <div>
         <NavBar/>
         <Banner/>
+        <div className="container w-75" >
+          <h1>
+            FUSION XIX Kickoff
+          </h1>
+          <p>
+            An annual dance showcase for UC San Diego students and dance teams.
+          </p>
+          <p className="font-weight-bold" >
+            This year’s event info:
+          </p>
+          <ul>
+            <li>
+              Date: Sunday, March 3rd
+            </li>
+            <li>
+              Time: 7:00 PM (Need official time.)
+            </li>
+            <li>
+              Location:&nbsp;
+              <a
+                href="http://universitycenters.ucsd.edu/aboutus_directions.php"
+              >
+                Price Center West Ballroom (Floor 2)
+              </a>
+            </li>
+          </ul>
+          <h1>
+            Line Up
+          </h1>
+          <p>
+            To be announced...
+          </p>
+        </div>
         <Footer/>
       </div>
       
