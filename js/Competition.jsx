253eaaada49eb698f15342fbb4ccbefa36eb2571
Simon <simonfong6@gmail.com>
2019-01-14 00:57:04 -0800

    Using componentfied LineUp and EventInfo

--- js/Competition.jsx
+++ js/Competition.jsx
@@ -6,34 +6,27 @@ class Competition extends React.Component {
   }
 
   render() {
+    const infoTexts = [
+      "FUSION Dance Competition (FUSION XIX)",
+      "Date: April 20th, 2019",
+      "Time: 5:00 PM"
+    ];
 
     return (
       <div>
         <NavBar/>
         <Banner/>
-        <div className="container w-50" >
-          <h1>
-            FUSION XIX
-          </h1>
-          <p className="font-weight-bold" >
-            This year’s event info:
-          </p>
-          <ul>
-            <li>
-              FUSION Dance Competition (FUSION XIX)
-            </li>
-            <li>
-              Date: April 20th, 2019
-            </li>
-            <li>
-              Location:&nbsp;
-              <a
-                href="https://www.google.com/maps/place/Golden+Hall/@32.717645,-117.163323,16z/data=!4m5!3m4!1s0x0:0x64836d4d79a1c138!8m2!3d32.7176451!4d-117.163323?hl=en-US"
-              >
-                Golden Hall
-              </a>
-            </li>
-          </ul>
+        <div className="container w-75" >
+          <EventInfo
+            title="FUSION XIX"
+            location="Golden Hall"
+            locationUrl="https://www.google.com/maps/place/Golden+Hall/@32.717645,-117.163323,16z/data=!4m5!3m4!1s0x0:0x64836d4d79a1c138!8m2!3d32.7176451!4d-117.163323?hl=en-US"
+            infoTexts={infoTexts}
+            description="One of the largest dance competitions on the West
+              Coast."
+          />
+          <WinnersList />
+          <LineUp />
         </div>
         <Footer/>
       </div>
