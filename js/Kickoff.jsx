de70a6c4035b5ce00c62fdc3e825954acc3accd6
Simon <simonfong6@gmail.com>
2019-01-14 01:01:25 -0800

    Using compnentfied LineUp and EventInfo

--- js/Kickoff.jsx
+++ js/Kickoff.jsx
@@ -6,43 +6,26 @@ class Kickoff extends React.Component {
   }
 
   render() {
+    const infoTexts = [
+      "Date: Sunday, March 3rd",
+      "Time: 7:00 PM (Need official time.)"
+    ];
 
     return (
       <div>
         <NavBar/>
         <Banner/>
+
         <div className="container w-75" >
-          <h2>
-            FUSION XIX Kickoff
-          </h2>
-          <p>
-            An annual dance showcase for UC San Diego students and dance teams.
-          </p>
-          <p className="font-weight-bold" >
-            This year’s event info:
-          </p>
-          <ul>
-            <li>
-              Date: Sunday, March 3rd
-            </li>
-            <li>
-              Time: 7:00 PM (Need official time.)
-            </li>
-            <li>
-              Location:&nbsp;
-              <a
-                href="http://universitycenters.ucsd.edu/aboutus_directions.php"
-              >
-                Price Center West Ballroom (Floor 2)
-              </a>
-            </li>
-          </ul>
-          <h2>
-            Line Up
-          </h2>
-          <p>
-            To be announced...
-          </p>
+          <EventInfo
+            title="FUSION XIX Kickoff"
+            location="Price Center West Ballroom (Floor 2)"
+            locationUrl="http://universitycenters.ucsd.edu/aboutus_directions.php"
+            infoTexts={infoTexts}
+            description="An annual dance showcase for UC San Diego students and
+              dance teams."
+          />
+          <LineUp />
         </div>
         <Footer/>
       </div>
