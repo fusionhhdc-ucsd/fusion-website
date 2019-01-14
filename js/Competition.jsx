278145ca5fed6ac9c03cbe40670a3b6f1aebd43f
Simon <simonfong6@gmail.com>
2019-01-13 23:21:28 -0800

    Removed map and moved style into bootstrap class

--- js/Competition.jsx
+++ js/Competition.jsx
@@ -11,20 +11,11 @@ class Competition extends React.Component {
       <div>
         <NavBar/>
         <Banner/>
-        <div
-          className='container'
-          style={{
-            'width': '70%'
-          }}
-        >
+        <div className="container w-50" >
           <h1>
             FUSION XIX
           </h1>
-          <p
-            style={{
-              'font-weight': 'bold'
-            }}
-          >
+          <p className="font-weight-bold" >
             This year’s event info:
           </p>
           <ul>
@@ -35,10 +26,14 @@ class Competition extends React.Component {
               Date: April 20th, 2019
             </li>
             <li>
-              Location: Golden Hall
+              Location:&nbsp;
+              <a
+                href="https://www.google.com/maps/place/Golden+Hall/@32.717645,-117.163323,16z/data=!4m5!3m4!1s0x0:0x64836d4d79a1c138!8m2!3d32.7176451!4d-117.163323?hl=en-US"
+              >
+                Golden Hall
+              </a>
             </li>
           </ul>
-          <LocationMap/>
         </div>
         <Footer/>
       </div>
