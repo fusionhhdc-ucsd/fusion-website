e0aa9c7db077d021844b17d8cebde5ba88c50018
Simon <simonfong6@gmail.com>
2019-01-13 21:41:59 -0800

    Style

--- js/navbar.jsx
+++ js/navbar.jsx
@@ -9,30 +9,60 @@ class NavBar extends React.Component {
 
     return (
       <nav className="navbar navbar-expand-md bg-dark navbar-dark">
-        <a className="navbar-brand" href="/index.html">FUSION</a>
-
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <a
+          className="navbar-brand"
+          href="/index.html"
+        >
+          FUSION
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button" data-toggle="collapse"
+          data-target="#collapsibleNavbar">
           <span className="navbar-toggler-icon"></span>
         </button>
-
-        <div className="collapse navbar-collapse" id="collapsibleNavbar">
+        <div
+          className="collapse navbar-collapse"
+          id="collapsibleNavbar"
+        >
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link" href="/index.html">Home</a>
+              <a
+                className="nav-link"
+                href="/index.html"
+              >
+                Home
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/about.html">About</a>
+              <a
+                className="nav-link"
+                href="/about.html"
+              >
+                About
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/staff.html">Our Staff</a>
+              <a
+                className="nav-link"
+                href="/staff.html"
+              >
+                Our Staff
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/contact.html">
+              <a
+                className="nav-link"
+                href="/contact.html"
+              >
                 Contact
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/fusion_xix.html">
+              <a
+                className="nav-link"
+                href="/fusion_xix.html"
+              >
                 FUSION XIX
               </a>
             </li>
