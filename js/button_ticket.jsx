31a325b37f4efb5db6329d6180be4cf718169d2e
Simon <simonfong6@gmail.com>
2019-03-04 21:47:49 -0800

    Added link to FUSION tickets and opens in new tab

--- js/button_ticket.jsx
+++ js/button_ticket.jsx
@@ -9,7 +9,10 @@ class ButtonTicket extends React.Component {
 
     return (
      <div className="text-center center-block">
-          <a href="#">
+          <a
+            href="https://www.eventbrite.com/e/fusion-xix-hip-hop-dance-competition-tickets-57350062581"
+            target="_blank"
+          >
             <button
               type="button"
               className="btn-lg btn-primary btn-ticket">
